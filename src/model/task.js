f196df22b17e3057be8cc9867c1ae75c4c11783f
Johang31m <johansegom08@gmail.com>
2023-02-21 09:26:09 -0500

    ultimo commit

--- src/model/task.js
+++ src/model/task.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 
 const TaskSchema = new Schema({
     title: { type: String, required: true },
-    description: { type: String}
+    description: { type: String},
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
