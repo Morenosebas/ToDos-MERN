4889755566e4e98a5857e903a7315f39b3cfd805
Johang31m <johansegom08@gmail.com>
2023-02-21 10:21:11 -0500

    update

--- src/app/app.js
+++ src/app/app.js
@@ -11,7 +11,6 @@ export default class App extends Component {
 
         return (
             <div>
-                {/*estas es mi nave */}
                 <Navigator />
                 <BlockStruct />
             </div>
--- src/db.js
+++ src/db.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://127.0.0.1:27017/mibasedeTask'
+const URI = 'mongodb://mongo:TfPtAn9mLKmZ9EcxrQGr@containers-us-west-94.railway.app:5472'
 
 mongoose
   .connect(URI)
   .then(db => console.log("DB is connected"))
   .catch(err => console.error(err));
-
  module.exports = mongoose;
